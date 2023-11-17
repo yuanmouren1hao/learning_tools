5bbc0b58fc604c2d208815f654389b7b1ada5eb8
yuanmouren1hao <342834599@qq.com>
2023-11-17 19:17:30 +0800

    c

---
+++ notify/check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+int=1
+while (($int <= 60)); do
+
+  if [ $((int % 5)) -eq 0 ]; then
+    `curl \
+        -d "Look ma, **bold text**, *italics*, ..." \
+        -H "Markdown: yes" \
+        ntfy.sh/goldenli-notify`
+  fi
+
+  let "int++"
+  sleep 1
+done
